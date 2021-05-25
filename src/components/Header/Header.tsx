325c62458e1a711e88d4199bede6d6119468b486
DarkShadow20 <kunal8011@gmail.com>
2021-05-25 23:44:49 +0530

    fix:header link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,10 @@
-import React from "react";
 import {Link} from "react-router-dom";
 import "./Header.css";
 export const Header=()=>{
-    // const logoutUser=()=>{
-    //     localStorage.removeItem("LogUser");
-    
-    // }
-    
     return (
         <>
             <div className="navbar navbar-dark bg-dark">
-                <Link to="/" className="header-link">
+                <Link to="/dashboard" className="header-link">
                     <p>NEOQUIZ</p>
                 </Link>
             </div>
